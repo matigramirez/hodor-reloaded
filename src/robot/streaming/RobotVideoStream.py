65163874721dbef327ba9f8bb7ca18f5ca74a065
Dario A. Marin Roth <dariomarinroth@gmail.com>
2024-11-25 00:35:36 -0300

    add client reconnection handling

--- src/robot/streaming/RobotVideoStream.py
+++ src/robot/streaming/RobotVideoStream.py
@@ -12,30 +12,40 @@ class RobotVideoStream:
         self.server_socket = None
         self.conn = None
         self.addr = None
-        self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+        self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90] #TODO: Agregar compresión a las settings
 
         if settings.video_stream_enable:
             self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.server_socket.bind(('0.0.0.0', self.settings.video_stream_port))
             self.server_socket.listen(1)
-            RobotLogger.info("Esperando conexión.")
-            self.conn, self.addr = self.server_socket.accept()
-            RobotLogger.info(f"Conexión establecida con {self.addr}.")
+            self._accept_connection
             return
         else:
             RobotLogger.warning("Streaming de video deshabilitado")
+
+    def _accept_connection(self):
+        RobotLogger.info("Esperando conexión.")
+        while True:
+            try:
+                self.conn, self.addr = self.server_socket.accept()
+                RobotLogger.info(f"Conexión establecida con {self.addr}.")
+                break
+            except Exception as e:
+                RobotLogger.error(f"Error al aceptar conexión: {e}")        
 
     def stream(self, frame):
         if not self.settings.video_stream_enable:
             return
 
         try:
-            _, encoded_frame = cv2.imencode('.jpg', frame, self.encode_param)
+            result, encoded_frame = cv2.imencode('.jpg', frame, self.encode_param)
             frame_bytes = encoded_frame.tobytes()
             size = len(frame_bytes)
             self.conn.sendall(struct.pack(">L", size) + frame_bytes)
-        except Exception as e:
-            RobotLogger.error(f"Error durante la transmisión: {e}")
+        except (BrokenPipeError, ConnectionResetError) as e:
+            RobotLogger.error(f"Conexión perdida con el cliente: {e}")
+            self.conn.close()
+            self._accept_connection()
 
     def close(self):
         self.conn.close()
